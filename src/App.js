715539bdbc391c5826a02d0dee4aab6a8e45aeec
Emika Ijuin <emikaijuin@gmail.com>
2019-01-22 17:50:51 +0800

    extract links to a navbar component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react"
+import { Route } from "react-router-dom"
+
 import HomePage from './pages/HomePage'
+import UserProfilePage from './pages/UserProfilePage'
+import Navbar from './components/Navbar'
 
+import './App.css'
 
-class App extends Component {
-
+class App extends React.Component {
   render() {
-
     return (
       <div>
-        <HomePage />
+        <Navbar />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/users/:id" component={UserProfilePage} />
       </div>
     )
   }
 }
 
-export default App;
+export default App
