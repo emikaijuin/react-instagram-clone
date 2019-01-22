60b010133e27a8fe45089d69e11c78fa0ffdfe1b
Emika Ijuin <emikaijuin@gmail.com>
2019-01-22 15:46:29 +0800

    extract homepage to separate component

---
+++ src/HomePage.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios'
+import UserImages from './UserImages'
+
+
+class HomePage extends Component {
+  state = {
+    users: [],
+    isLoading: true
+  }
+
+  componentDidMount() {
+    axios.get('https://insta-oct18.nextacademy.com/api/v1/users')
+    .then( result => {
+        this.setState({
+          users: result.data,
+          isLoading: false})
+      }
+    ).catch(
+      error => {
+        console.log('ERROR ', error)
+      }
+    )
+  }
+
+  returnUsers = () => {
+    return (
+      <ul>
+        {
+          this.state.users.map(user =>
+            <li>
+              <UserImages id={user.id} />
+              {user.id}: {user.username}
+            </li>
+          )
+        }
+      </ul>
+    )
+  }
+
+  returnLoading = () => {
+    return (
+      <img src="loading.gif"></img>
+    )
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h1>Home Pages</h1>
+        { this.state.isLoading ? this.returnLoading() : this.returnUsers() }
+      </div>
+    )
+  }
+}
+
+export default HomePage;
