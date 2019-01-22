2ac24ea8d2219cf2b8e14ce4624cb54eafbf2490
Emika Ijuin <emikaijuin@gmail.com>
2019-01-22 14:52:23 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -3,25 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [
+      {
+        id: 1,
+        username: "blake",
+        profileImage: "http://next-curriculum-instagram.s3.amazonaws.com/idol2-blake.jpg"
+      },
+      {
+        id: 2,
+        username: "ryanG",
+        profileImage: "http://next-curriculum-instagram.s3.amazonaws.com/idol1-ryan.jpg"
+      },
+      {
+        id: 3,
+        username: "bigfan",
+        profileImage: "http://next-curriculum-instagram.s3.amazonaws.com/bigfan-9AE7468E-4C35-41D1-AA68-31939891B5E1.png"
+      }
+    ]
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Home Pages</h1>
+        <ul>
+          {
+            this.state.users.map(user =>
+              <li>
+                <img src={user.profileImage}></img>
+                {user.id}: {user.username}
+              </li>
+            )
+          }
+        </ul>
       </div>
-    );
+    )
   }
 }
 
