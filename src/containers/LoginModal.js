b19355d71989af9113f6f6db5b723d5b78857cef
Emika Ijuin <emikaijuin@gmail.com>
2019-01-23 16:12:58 +0800

    Add validation to form so it cannot submit without valid email and password

--- src/containers/LoginModal.js
+++ src/containers/LoginModal.js
@@ -2,16 +2,68 @@ import React, {Component} from "react";
 
 class LoginModal extends Component {
 
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    if (this.isSubmittable()) {
+      alert('Welcome to Nextagram, ' + this.state.email)
+      this.props.unmountMe()
+    } else {
+      alert('Not a valid email, please try again.')
+    }
+  }
+
+  isSubmittable = () => this.isValidEmail() && this.state.password ? true : false
+
+  handleEmailInput = event => {
+    this.setState({ email: event.target.value })
+  }
+
+  handlePasswordInput = event => {
+    this.setState({ password: event.target.value })
+  }
+
+  isValidEmail() {
+    if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(this.state.email)) {
+      return true 
+    } else {
+      return false
+    }
+  }
 
   render() {
     return (
       <div className="login-modal" >
-        <form >
+        <form onSubmit={ this.handleSubmit }>
           <div><label>Email</label></div>
-          <div><input type="email" name="user[email]" /></div>
+          <div>
+            <input 
+              type= "text" 
+              name= "user[email]" 
+              value= { this.state.email }
+              onChange= { this.handleEmailInput }
+            />
+          </div>
           <div><label>Password</label></div>
-          <div><input type="password" name="user[password]" /></div>
-          <div><input type="submit" /></div>
+          <div>
+            <input 
+              type="password" 
+              name="user[password]"
+              value = { this.state.password }
+              onChange = { this.handlePasswordInput }
+            />
+          </div>
+          <div>
+            <input 
+              type ="submit" 
+              disabled = { !this.isSubmittable() }
+            />
+
+          </div>
         </form>
         <div onClick={this.props.unmountMe}>Close</div>
       </div>
